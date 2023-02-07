9b06530066ab8a266360195ce90b3be37d4589b6
jacobpclouse <clouse.jp@gmail.com>
2023-02-07 10:35:19 -0500

    reorganized the try except statement so that it prints out unique statements if it works vs if it fails and not both

--- main.py
+++ main.py
@@ -8,9 +8,10 @@ def Download(link):
   youtubeObject = youtubeObject.streams.get_highest_resolution()
   try:
       youtubeObject.download()
+      print("This download has completed! Yahooooo!")
   except:
     print("There has been an error in downloading your youtube video")
-  print("This download has completed! Yahooooo!")
+  
 
 link = input("Put your youtube link here!! URL: ")
 Download(link)

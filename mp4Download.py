41fd091a14f38cc4076d9e68aa9b4a3f16b3aafb
jc366512 <jclouse@albany.edu>
2023-05-03 10:44:10 -0400

    edited the alt download so that it downloads as mp4 files

---
+++ mp4Download.py
@@ -0,0 +1,17 @@
+import yt_dlp
+
+# NOTE: You need to have ffmpeg downloaded and added to the path variable for this to work, get it here: https://ffmpeg.org/download.html#build-windows
+# You also need to 'pip install yt-dlp'
+
+# Set the URL of the YouTube video you want to download
+url = input("Give me a URL: ")
+
+# Set options for the downloader
+ydl_opts = {
+    'format': 'mp4',
+    'outtmpl': '%(title)s.%(ext)s',
+}
+
+# Create the downloader and pass in the options and URL
+with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+    ydl.download([url])
